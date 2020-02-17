85a76e40265391940b64d4584109bd8434e67d27
ELBARNAKI <60877033+ELBARNAKI@users.noreply.github.com>
2020-02-17 10:31:52 +0100

    Add files via upload

---
+++ test2.js
@@ -0,0 +1,16 @@
+ let http =require('http');
+ let port = 3000;
+
+ let server = http.createServer(function(request,response){
+     console.log('Got request');
+     response.end('heloo this is a test ');
+ });
+ server.listen(port, function(error){
+     if (error){
+         return console.log(' Oups error ',error);
+     }
+     console.log('Server is listening on port: ', port);
+ });
+
+
+ 
